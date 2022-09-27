49c94f5d9d6e76d9f5a84322290c7ec38d0994fd
tank <tank@certifiedcoderz.com>
2022-09-27 15:46:42 -0400

    good

--- .biz9_config.sh
+++ .biz9_config.sh
@@ -1,9 +1,9 @@
 BIZ9_SERVICE_VERSION='3.5.7'
-APP_VERSION='1.6.5'
+APP_VERSION='1.6.6'
 APP_ID="19";
 APP_TITLE="BiZ9-Service";
 APP_TITLE_ID='ser19';
 EC2_IP_ADDR="0.00.000.000"
 EC2_DEPLOY_DIR="/home/admin/www/biz92"
 EC2_KEY_FILE="other/aws/ec2_key/_app_id_.pem"
-REPO_URL="git@github.com:bossappz/biz9-service-unstable.git";
+REPO_URL="git@github.com:bossappz/biz9-service-stable.git";
